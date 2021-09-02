79d05e250633d78ce5ebc8d72401a2cc6b21019d
Braiden Hook <bghook@smu.edu>
2021-09-02 16:56:51 -0500

    #1

--- DSString.cpp
+++ DSString.cpp
@@ -2,4 +2,65 @@
 // Created by mark on 6/2/2021.
 //
 
-#include "DSString.h"
+/*#include "DSString.h"
+
+DSString::DSString() {
+
+}
+
+DSString::DSString(const char *) {
+
+}
+
+DSString::DSString(const DSString &) {
+
+}
+
+DSString::~DSString() {
+
+}
+
+DSString &DSString::operator=(const char *) {
+    return ;
+}
+
+/DSString &DSString::operator=(const DSString &) {
+    return ;
+}
+
+DSString DSString::operator+(const DSString &) {
+    return DSString();
+}
+
+bool DSString::operator==(const char *) {
+    return false;
+}
+
+bool DSString::operator==(const DSString &) {
+    return false;
+}
+
+bool DSString::operator>(const DSString &) {
+    return false;
+}
+
+bool DSString::operator>(const char *) {
+    return false;
+}
+
+char &DSString::operator[](const int) {
+    return <#initializer#>;
+}
+
+DSString DSString::substring(int start, int numChars) {
+    return DSString();
+}
+
+char *DSString::c_str() {
+    return nullptr;
+}
+
+std::ostream &operator<<(std::ostream &, const DSString &) {
+    return <#initializer#>;
+}
+*/
--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include "DSString.h"
+using namespace std;
+int main(int argc, char** argv) {
 
-int main() {
+    for(int i = 0; i < argc; i++){
+        cout <<"argv["<< i << "]: " << argv[i] << endl;
+    }
+
+    ifstream file1 (argv[1]);
+
+    if (file1.is_open()){
+        char char1[500];
+        char char2[1000];
+        char char3[1000];
+
+        file1.getline(char1, 500, '\n');
+        file1.getline(char2, 1000, '\n');
+        file1.getline(char3, 1000, '\n');
+
+
+        /**
+        This goes into the tweet object!
+
+        DSString data;
+
+        file1.getline(data, 1000, '\n');
+
+         **/
+
+
+        cout << endl;
+
+        cout << char1 << endl;
+        cout << char2 << endl;
+        cout << char3 << endl;
+
+
+        //while (file1.getline(char1, 100)){
+        //    file1.getline(char1, 100);
+       // }
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
